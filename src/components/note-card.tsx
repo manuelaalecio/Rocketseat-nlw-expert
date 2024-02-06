c516eb16f35830212dca93002ac84f32bb6c3e84
manuela-alecio <manuelaalecio.dev@gmail.com>
2024-02-06 14:20:21 -0300

    feat(api): add date-fns package

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface NoteCardProps {
   note: {
@@ -12,7 +14,7 @@ export function NoteCard({ note }: NoteCardProps) {
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toISOString()}
+          {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
         </span>
         <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
@@ -23,7 +25,7 @@ export function NoteCard({ note }: NoteCardProps) {
         <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
           <div className="flex flex-1 flex-col gap-3 p-5">
             <span className="text-sm font-medium text-slate-300">
-              {note.date.toISOString()}
+              {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
             </span>
             <p className="text-sm leading-6 text-slate-400">{note.content}</p>
           </div>
