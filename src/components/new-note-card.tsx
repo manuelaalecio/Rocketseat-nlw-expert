5fa3b88ebfa656e1fe293212300e025aaffe7cc4
manuela-alecio <manuelaalecio.dev@gmail.com>
2024-02-06 15:51:06 -0300

    fix: fix the modal issue

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,12 +1,18 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 
 export function NewNoteCard() {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
 
   function handleStartEditor() {
     setShouldShowOnboarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    if (event.target.value === "") {
+      setShouldShowOnboarding(true);
+    }
   }
 
   return (
@@ -34,11 +40,11 @@ export function NewNoteCard() {
             </span>
             {shouldShowOnboarding ? (
               <p className="text-sm leading-6 text-slate-400">
-                Comece
+                Comece{" "}
                 <button className="font-medium text-lime-400 hover:underline">
                   gravando uma nota
-                </button>
-                em áudio ou se preferir
+                </button>{" "}
+                em áudio ou se preferir{" "}
                 <button
                   onClick={handleStartEditor}
                   className="font-medium text-lime-400 hover:underline"
@@ -51,6 +57,7 @@ export function NewNoteCard() {
               <textarea
                 autoFocus
                 className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                onChange={handleContentChanged}
               />
             )}
           </div>
