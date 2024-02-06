d1267ad7d899f762ac2ade1d08b7ef0fae7d313a
manuela-alecio <manuelaalecio.dev@gmail.com>
2024-02-06 13:50:02 -0300

    feat: add props to note cards

--- src/app.tsx
+++ src/app.tsx
@@ -19,9 +19,12 @@ export function App() {
 
       <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
         <NewNoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: "Teste",
+          }}
+        />
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,13 +1,17 @@
-export function NoteCard() {
+interface NoteCardProps {
+  note: {
+    date: Date;
+    content: string;
+  };
+}
+
+export function NoteCard({ note }: NoteCardProps) {
   return (
-    <button className="rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
-      <span className="text-sm font-medium text-slate-300">há 4 dias</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
-        architecto minima perspiciatis rem aliquam cupiditate recusandae sunt
-        dolores amet! Incidunt quibusdam dolore officiis repellendus provident?
-        Quisquam modi dignissimos error expedita.
-      </p>
+    <button className="rounded-md text-left flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+      <span className="text-sm font-medium text-slate-300">
+        {note.date.toISOString()}
+      </span>
+      <p className="text-sm leading-6 text-slate-400">{note.content}</p>
 
       <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
     </button>
