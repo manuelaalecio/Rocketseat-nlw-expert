bf3a9b52df45f4d13540cbc015ce26a2e2fe820c
manuela-alecio <manuelaalecio.dev@gmail.com>
2024-02-07 18:04:06 -0300

    feat: add toaster when press button to save note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
 import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
 
 export function NewNoteCard() {
   const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true);
@@ -21,6 +22,7 @@ export function NewNoteCard() {
   function handleSaveNote(event: FormEvent) {
     event.preventDefault();
     console.log(content);
+    toast.success("Nota salva com sucesso!");
   }
 
   return (
